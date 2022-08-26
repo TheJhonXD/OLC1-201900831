23b31f9574c165286e850aee836d16caf7f90868
Jhonatan <3227943820801@ingenieria.usac.edu.gt>
2022-08-26 01:11:01 -0600

    Interfaz
    Abrir y guardar archivo

---
+++ Proyecto 1/Proyecto1OLC/src/proyecto1olc/Proyecto1OLC.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto1olc;
+
+/**
+ *
+ * @author TheJhonX
+ */
+public class Proyecto1OLC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
