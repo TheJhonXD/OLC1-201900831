607a24eaf652363f98880031d5ddc93089af9e1d
Jhon <>
2022-10-08 18:39:16 -0600

    inicio server node

---
+++ Proyecto 2/server/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+const corsOptions = { origin: true, optionsSuccessStatus: 200 };
+const port = 8080;
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(express.urlencoded({extended: true}));
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+});
+
+app.get('/', (req, res) => {
+    res.json({mensaje:"hola mundo"})
+});
+
+app.get('/texto', (req, res) => {
+    res.send("hola mundo jsjsjsj")
+});
