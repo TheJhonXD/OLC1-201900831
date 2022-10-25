4c62c9783bc3541e3a79017126407c398f0d4b42
Jhonatan <3227943820801@ingenieria.usac.edu.gt>
2022-10-25 00:47:57 -0600

    iniciado abstracta

---
+++ Proyecto 2/server/src/index.ts
@@ -0,0 +1,19 @@
+const parser = require('./grammar/gramatica.js');
+const fs = require('fs');
+
+try {
+    const entrada = fs.readFileSync("src/entrada.txt");
+    const ast = parser.parse(entrada.toString());
+
+    for (const instruccion of ast) {
+        try {
+            instruccion.ejecutar();
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+} catch (error) {
+    console.log(error);
+    
+}
